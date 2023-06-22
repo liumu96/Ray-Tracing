b1668a6378fbbe43c6c2559fd6573a8264de3d6d
liumu96 <918883717@qq.com>
2023-06-22 15:44:49 +0800

    correct lambertian

--- RayTracingInOneWeekend/RayTracingInOneWeekend/vec3.h
+++ RayTracingInOneWeekend/RayTracingInOneWeekend/vec3.h
@@ -111,7 +111,7 @@ inline vec3 unit_vector(vec3 v) {
     return v / v.length();
 }
 
-vec3 random_in_unit_sphere() {
+inline vec3 random_in_unit_sphere() {
     while (true) {
         auto p = vec3::random(-1, 1);
         if (p.length_squared() >= 1) continue;
@@ -119,6 +119,8 @@ vec3 random_in_unit_sphere() {
     }
 }
 
-
+inline vec3 random_unit_vector() {
+    return unit_vector(random_in_unit_sphere());
+}
 
 #endif /* vec3_h */
