ae49f016e85e9e582376cd34ac2d1885ec0b4f74
liumu96 <918883717@qq.com>
2023-06-26 13:35:39 +0800

    BVH

--- RayTracingTheNextWeek/RayTracingTheNextWeek/hittable.h
+++ RayTracingTheNextWeek/RayTracingTheNextWeek/hittable.h
@@ -10,6 +10,7 @@
 
 #include "ray.h"
 #include "rtweekend.h"
+#include "aabb.h"
 
 class material;
 
@@ -18,6 +19,8 @@ struct hit_record {
     vec3 normal;
     shared_ptr<material> mat_ptr;
     double t;
+    double u;
+    double v;
     bool front_face;
     
     inline void set_face_normal(const ray& r, const vec3& outward_normal) {
@@ -29,6 +32,7 @@ struct hit_record {
 class hittable {
 public:
     virtual bool hit(const ray& r, double t_min, double t_max, hit_record& rec) const = 0;
+    virtual bool bounding_box(double time0, double time1, aabb& output_box) const = 0;
 };
 
 #endif /* hittable_h */
--- RayTracingTheNextWeek/RayTracingTheNextWeek/material.h
+++ RayTracingTheNextWeek/RayTracingTheNextWeek/material.h
@@ -9,6 +9,7 @@
 #define material_h
 
 #include "rtweekend.h"
+#include "texture.h"
 
 struct hit_record;
 
@@ -19,7 +20,8 @@ public:
 
 class lambertian : public material {
 public:
-    lambertian(const color& a) : albedo(a) {}
+    lambertian(const color& a) : albedo(make_shared<solid_color>(a)) {}
+    lambertian(shared_ptr<texture> a) : albedo(a) {}
     
     virtual bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered) const override {
         auto scatter_direction = rec.normal + random_unit_vector();
@@ -29,12 +31,12 @@ public:
             scatter_direction = rec.normal;
         
         scattered = ray(rec.p, scatter_direction, r_in.time());
-        attenuation = albedo;
+        attenuation = albedo->value(rec.u, rec.v, rec.p);
         return true;
     }
     
 public:
-    color albedo;
+    shared_ptr<texture> albedo;
 };
 
 class metal : public material {
