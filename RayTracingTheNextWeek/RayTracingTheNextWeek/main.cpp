a08ffffce88b020cfd953886af7e9b5ad708ba9e
liumu96 <918883717@qq.com>
2023-06-26 13:44:34 +0800

    Solid Textures

--- RayTracingTheNextWeek/RayTracingTheNextWeek/main.cpp
+++ RayTracingTheNextWeek/RayTracingTheNextWeek/main.cpp
@@ -16,6 +16,17 @@
 
 #include <iostream>
 
+hittable_list two_spheres() {
+    hittable_list objects;
+    
+    auto checker = make_shared<checker_texture>(color(0.2, 0.3, 0.1), color(0.9, 0.9, 0.9));
+    
+    objects.add(make_shared<sphere>(point3(0, -10, 0), 10, make_shared<lambertian>(checker)));
+    objects.add(make_shared<sphere>(point3(0, 10, 0), 10, make_shared<lambertian>(checker)));
+    
+    return objects;
+}
+
 // A Final Scene
 hittable_list random_scene() {
     hittable_list world;
@@ -94,19 +105,40 @@ int main() {
 //    const int image_height = static_cast<int>(image_width / aspect_ratio);
     const int samples_per_pixel = 100;
     const int max_depth = 50;
-
+    
     // World
-    auto world = random_scene();
+    hittable_list world;
+    
+    point3 lookfrom;
+    point3 lookat;
+    auto vfov = 40.0;
+    auto aperture = 0.0;
+    
+    switch (0) {
+        case 1:
+            world = random_scene();
+            lookfrom = point3(13, 2, 3);
+            lookat = point3(0, 0, 0);
+            vfov = 20.0;
+            aperture = 0.1;
+            break;
+            
+        default:
+        case 2:
+            world = two_spheres();
+            lookfrom = point3(13, 2, 3);
+            lookat = point3(0, 0, 0);
+            vfov = 20.0;
+            break;
+            
+    }
     
     // Camera
-    point3 lookfrom(13, 2, 3);
-    point3 lookat(0, 0, 0);
     vec3 vup(0, 1, 0);
     auto dist_to_focus = 10.0;
-    auto aperture = 0.1;
     int image_height = static_cast<int>(image_width / aspect_ratio);
     
-    camera cam(lookfrom, lookat, vup, 20, aspect_ratio, aperture, dist_to_focus, 0.0, 1.0);
+    camera cam(lookfrom, lookat, vup, vfov, aspect_ratio, aperture, dist_to_focus, 0.0, 1.0);
     
     // Render
     
