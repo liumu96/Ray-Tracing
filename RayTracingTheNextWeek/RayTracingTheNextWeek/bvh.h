4ca472420efa1302afd057f51cbf71aaf5c1494b
liumu96 <918883717@qq.com>
2023-06-27 20:10:57 +0800

    cornell block

--- RayTracingTheNextWeek/RayTracingTheNextWeek/bvh.h
+++ RayTracingTheNextWeek/RayTracingTheNextWeek/bvh.h
@@ -48,13 +48,35 @@ bool bvh_node::hit(const ray& r, double t_min, double t_max, hit_record& rec) co
         return false;
     
     bool hit_left = left->hit(r, t_min, t_max, rec);
-    bool hit_right = right-<hit(r, t_min, hit_left ? rec.t : t_max, rec);
+    bool hit_right = right->hit(r, t_min, hit_left ? rec.t : t_max, rec);
     
     return hit_left || hit_right;
 }
 
-bvh_node::bvh_node(std::vector<shared_ptr<hittable>>& src_objects, size_t start, size_t end, double time0, double time1) {
-    auto objects = stc_objects; // Create a modifiable array of the source scene objects
+inline bool box_compare(const shared_ptr<hittable> a, const shared_ptr<hittable> b, int axis) {
+    aabb box_a;
+    aabb box_b;
+    
+    if (!a->bounding_box(0, 0, box_a) || !b->bounding_box(0, 0, box_b))
+        std::cerr << "No bounding box in bvh_node constructor.\n";
+        
+        return box_a.min().e[axis] < box_b.min().e[axis];
+}
+
+bool box_x_compare(const shared_ptr<hittable> a, const shared_ptr<hittable> b) {
+    return box_compare(a, b, 0);
+}
+
+bool box_y_compare(const shared_ptr<hittable> a, const shared_ptr<hittable> b) {
+    return box_compare(a, b, 1);
+}
+
+bool box_z_compare(const shared_ptr<hittable> a, const shared_ptr<hittable> b) {
+    return box_compare(a, b, 2);
+}
+
+bvh_node::bvh_node(const std::vector<shared_ptr<hittable>>& src_objects, size_t start, size_t end, double time0, double time1) {
+    auto objects = src_objects; // Create a modifiable array of the source scene objects
     
     int axis = random_int(0, 2);
     auto comparator = (axis == 0) ? box_x_compare : (axis == 1) ? box_y_compare : box_z_compare;
@@ -86,28 +108,6 @@ bvh_node::bvh_node(std::vector<shared_ptr<hittable>>& src_objects, size_t start,
     
     box = surrounding_box(box_left, box_right);
     
-}
-
-inline bool box_compare(const shared_ptr<hittable> a, const shared_ptr<hittable> b, int axis) {
-    aabb box_a;
-    aabb box_b;
-    
-    if (!a->bounding_box(0, 0, box_a) || !b->bounding_box(0, 0, box_b))
-        std::cerr << "No bounding box in bvh_node constructor.\n;
-        
-        return box_a.min().e[axis] < box_b.min().e[axis];
-}
-
-bool box_x_compare(const shared_ptr<hittable> a, const shared_ptr<hittable> b) {
-    return box_compare(a, b, 0);
-}
-
-bool box_y_compare(const shared_ptr<hittable> a, const shared_ptr<hittable> b) {
-    return box_compare(a, b, 1);
-}
-
-bool box_z_compare(const shared_ptr<hittable> a, const shared_ptr<hittable> b) {
-    return box_compare(a, b, 2);
 }
 
 
--- RayTracingTheNextWeek/RayTracingTheNextWeek/material.h
+++ RayTracingTheNextWeek/RayTracingTheNextWeek/material.h
@@ -15,6 +15,9 @@ struct hit_record;
 
 class material {
 public:
+    virtual color emitted(double u, double v, const point3& p) const {
+        return color(0, 0, 0);
+    }
     virtual bool scatter( const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered) const = 0;
 };
 
@@ -92,4 +95,37 @@ private:
     }
 };
 
+class diffuse_light : public material {
+public:
+    diffuse_light(shared_ptr<texture> a) : emit(a) {}
+    diffuse_light(color c) : emit(make_shared<solid_color>(c)) {}
+    
+    virtual bool scatter(const ray &r_in, const hit_record &rec, color &attenuation, ray &scattered) const override {
+        return false;
+    }
+    
+    virtual color emitted(double u, double v, const point3& p) const override {
+        return emit->value(u, v, p);
+    }
+    
+public:
+    shared_ptr<texture> emit;
+};
+
+class isotropic : public material {
+public:
+    isotropic(color c) : albedo(make_shared<solid_color>(c)) {}
+    isotropic(shared_ptr<texture> a) : albedo(a) {}
+
+    virtual bool scatter(
+        const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered
+    ) const override {
+        scattered = ray(rec.p, random_in_unit_sphere(), r_in.time());
+        attenuation = albedo->value(rec.u, rec.v, rec.p);
+        return true;
+    }
+
+public:
+    shared_ptr<texture> albedo;
+};
 #endif /* material_h */
